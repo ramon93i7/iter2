cd8d82e7fd2d1be8d3e377de89a989d7d628432c
Roman Shemyakin <ramon93i7@gmail.com>
2019-09-16 00:15:05 +0300

    algorithms.splitting: generalization, `collect_to` and `wrap_into`

--- iter2/algorithms/splitting.py
+++ iter2/algorithms/splitting.py
@@ -20,8 +20,11 @@ itertools__tee = itertools.tee
 operator__itemgetter = operator.itemgetter
 
 
+UNDEFINED = object()
+
+
 @export_from_module
-def distribute(iterable, n):
+def distribute(iterable, n, *, collect_to=tuple, wrap_into=UNDEFINED):
     '''
     Distributes items from `iterable` among `n` smaller iterables.
 
@@ -34,10 +37,13 @@ def distribute(iterable, n):
     ((1, 4), (2, 5), (3,))
     '''
     copies = itertools__tee(iterable, n)
-    return tuple(
+    result = (
         itertools__islice(copy, idx, None, n)
         for idx, copy in builtin__enumerate(copies)
     )
+    if wrap_into is not UNDEFINED:
+        result = builtin__map(wrap_into, result)
+    return collect_to(result)
 
 
 @export_from_module
@@ -60,7 +66,7 @@ def partition(iterable, predicate):
 
 
 @export_from_module
-def split_after(iterable, predicate):
+def split_after(iterable, predicate, *, wrap_into=UNDEFINED):
     '''
     Splits `iterable` after every `predicate`-positive item into sub-iterators.
 
@@ -82,12 +88,18 @@ def split_after(iterable, predicate):
             else:
                 yield item
 
-    while spy_it.spy().is_ok:
-        yield split_chunk_gen(spy_it, predicate)
+    def split_chunk_cycle(spy_it, predicate):
+        while spy_it.spy().is_ok:
+            yield split_chunk_gen(spy_it, predicate)
+
+    result = split_chunk_cycle(spy_it, predicate)
+    if wrap_into is not UNDEFINED:
+        result = builtin__map(wrap_into, result)
+    return result
 
 
 @export_from_module
-def split_at(iterable, predicate):
+def split_at(iterable, predicate, *, wrap_into=UNDEFINED):
     '''
     Splits `iterable` on every `predicate`-positive item into sub-iterators. Delimiting items are not included.
 
@@ -99,10 +111,10 @@ def split_at(iterable, predicate):
     >>> tuple(map(tuple, split_at([1, 2, 0, 4, 5, 0], bool)))
     ((1, 2), (4, 5), ())
     '''
-    spy_it = SpyIterator(iterable)
-    previously_splitted = Flag()
 
-    def split_chunk_gen(it, pred):
+    # TODO: there is a bug w/ non sequential access to chunks - general problem "Dependent Iterators Problem"
+
+    def split_chunk_gen(it, pred, previously_splitted):
         for item in it:
             if pred(item):
                 previously_splitted.set()
@@ -110,13 +122,21 @@ def split_at(iterable, predicate):
             else:
                 yield item
 
-    while spy_it.spy().is_ok or previously_splitted:
-        previously_splitted.clear()
-        yield split_chunk_gen(spy_it, predicate)
+    def split_chunk_cycle(iterable, predicate):
+        spy_it = SpyIterator(iterable)
+        previously_splitted = Flag()
+        while spy_it.spy().is_ok or previously_splitted:
+            previously_splitted.clear()
+            yield split_chunk_gen(spy_it, predicate, previously_splitted)
+
+    result = split_chunk_cycle(iterable, predicate)
+    if wrap_into is not UNDEFINED:
+        result = builtin__map(wrap_into, result)
+    return result
 
 
 @export_from_module
-def split_before(iterable, predicate):
+def split_before(iterable, predicate, *, wrap_into=UNDEFINED):
     '''
     Splits `iterable` before every `predicate`-positive item into sub-iterators.
 
@@ -128,8 +148,7 @@ def split_before(iterable, predicate):
     >>> tuple(map(''.join, split_before("Ok!Let's go!", str.isupper)))
     ('Ok!', "Let's go!")
     '''
-    spy_it = SpyIterator(iterable)
-
+    # TODO: "Dependent Iterators Problem"
     def split_chunk_gen(spy_it, pred):
         ok, item = spy_it.spy()
         if pred(item):
@@ -142,12 +161,19 @@ def split_before(iterable, predicate):
             yield item
             ok, item = spy_it.spy()
 
-    while spy_it.spy().is_ok:
-        yield split_chunk_gen(spy_it, predicate)
+    def split_chunk_cycle(iterable, predicate):
+        spy_it = SpyIterator(iterable)
+        while spy_it.spy().is_ok:
+            yield split_chunk_gen(spy_it, predicate)
+
+    result = split_chunk_cycle(iterable, predicate)
+    if wrap_into is not UNDEFINED:
+        result = builtin__map(wrap_into, result)
+    return result
 
 
 @export_from_module
-def unzip(iterable, arity=2):
+def unzip(iterable, arity=2, collect_to=tuple, wrap_into=UNDEFINED):
     '''
     Produces `arity`-count of iterators from `iterable`. i-th iterator yields every i-th element from `arity`-length item.
 
@@ -161,13 +187,11 @@ def unzip(iterable, arity=2):
     '''
     if not isinstance(arity, int) or arity < 2:
         raise TypeError('`arity` must be integer greater or equal to 2')
-    if arity == 2:
-        copy1, copy2 = itertools__tee(iterable, 2)
-        first_flow = (first for first, _ in copy1)
-        second_flow = (second for _, second in copy2)
-        return first_flow, second_flow
-    else:
-        return tuple(
-            (builtin__map(operator__itemgetter(idx), copy)
-             for idx, copy in builtin__enumerate(itertools__tee(iterable, arity)))
-        )
+    result = (
+        (builtin__map(operator__itemgetter(idx), copy)
+         for idx, copy in builtin__enumerate(itertools__tee(iterable, arity)))
+    )
+    if wrap_into is not UNDEFINED:
+        result = builtin__map(wrap_into, result)
+    return collect_to(result)
+
